820dab297056a02e8021cb708a22f7f6aea8a69a
jinwon1234 <ljw0626@naver.com>
2025-11-23 17:22:27 +0900

    feat: 멀티턴 기능 구현

--- src/main/java/wowa/myqna/chat/service/ChatService.java
+++ src/main/java/wowa/myqna/chat/service/ChatService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.api.Advisor;
 import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.messages.AssistantMessage;
 import org.springframework.ai.chat.messages.MessageType;
 import org.springframework.ai.chat.messages.SystemMessage;
@@ -16,12 +17,15 @@ import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import wowa.myqna.chat.dto.ChatRequestDto;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class ChatService {
 
     private final ChatClient chatClient;
     private final VectorStore vectorStore;
+    private final ChatMemory chatMemory;
 
     public Flux<String> generateStream(ChatRequestDto chatRequestDto) {
 
@@ -29,6 +33,7 @@ public class ChatService {
         String userId = chatRequestDto.userId();
 
         UserMessage userMessage = new UserMessage(text);
+        chatMemory.add(userId, userMessage);
 
         QuestionAnswerAdvisor ragAdvisor = QuestionAnswerAdvisor.builder(vectorStore)
                 .searchRequest(SearchRequest.builder()
@@ -38,11 +43,20 @@ public class ChatService {
                         .build())
                 .build();
 
-        Prompt prompt = new Prompt(userMessage);
+        Prompt prompt = new Prompt(chatMemory.get(userId));
+
+        StringBuffer messageBuffer = new StringBuffer();
 
         return chatClient.prompt(prompt)
                 .advisors(ragAdvisor)
                 .stream()
-                .content();
+                .content()
+                .mapNotNull(token-> {
+                    messageBuffer.append(token);
+                    return token;
+                })
+                .doOnComplete(() -> {
+                    chatMemory.add(userId, new AssistantMessage(messageBuffer.toString()));
+                });
     }
 }
--- src/main/java/wowa/myqna/config/AIConfig.java
+++ src/main/java/wowa/myqna/config/AIConfig.java
@@ -1,12 +1,34 @@
 package wowa.myqna.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.ChatMemoryRepository;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 public class AIConfig {
+
+    @Bean
+    public ChatMemoryRepository chatMemoryRepository(JdbcTemplate jdbcTemplate, PlatformTransactionManager transactionManager) {
+        return JdbcChatMemoryRepository.builder()
+                .jdbcTemplate(jdbcTemplate)
+                .transactionManager(transactionManager)
+                .build();
+    }
+
+    @Bean
+    public ChatMemory chatMemory(ChatMemoryRepository chatMemoryRepository) {
+        return MessageWindowChatMemory.builder()
+                .maxMessages(10)
+                .chatMemoryRepository(chatMemoryRepository)
+                .build();
+    }
 
     @Bean
     public ChatClient openAiChatClient(OpenAiChatModel openAiChatModel) {
