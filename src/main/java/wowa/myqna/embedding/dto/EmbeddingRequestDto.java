18c223ec468f0e1002d501a5324f6e369a1704dd
jinwon1234 <ljw0626@naver.com>
2025-11-24 16:48:06 +0900

    refactor: request dto validation 및 예외처리 보완

--- src/main/java/wowa/myqna/embedding/dto/EmbeddingRequestDto.java
+++ src/main/java/wowa/myqna/embedding/dto/EmbeddingRequestDto.java
@@ -5,9 +5,9 @@ import jakarta.validation.constraints.NotNull;
 import org.springframework.web.multipart.MultipartFile;
 
 public record EmbeddingRequestDto(
-        @NotBlank
+        @NotBlank(message = "이름은 필수값입니다.")
         String myName,
-        @NotNull
+        @NotNull(message = "PDF 파일은 필수값입니다.")
         MultipartFile multipartFile
 ) {
 }
